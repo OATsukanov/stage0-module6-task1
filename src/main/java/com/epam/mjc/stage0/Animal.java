b292b2d5e872a7b0d85ec3ac945e8d01d97daff9
oleg <tsukanov.regula@gmail.com>
2023-01-18 18:36:31 +0500

    first try

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,42 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public String getDescription() {
+
+        String furExpression = (getHasFur() == true)? "a" : "no";
+
+        String pawnExpression = (getNumberOfPaws() == 1)? "paw" : "paws";
+
+        String description = "This animal is mostly " + getColor() +". It has " + getNumberOfPaws() + " " + pawnExpression +
+                " and " + furExpression + " fur.";
+
+        return description;
+
+
+    }
+
+    public String getColor() {
+
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+
+        return numberOfPaws;
+    }
+
+    public boolean getHasFur() {
+
+        return hasFur;
+    }
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,26 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+
+    private int numberOfWings;
+    private boolean canFly;
+
+
+    @Override
+    public String getDescription() {
+
+        String furExpression = (getHasFur() == true)? "a" : "no";
+        String pawnExpression = (getNumberOfPaws() == 1)? "paw" : "paws";
+
+        String description = "This animal is mostly " + getColor() +". It has " + getNumberOfPaws() + " " + pawnExpression +
+                " and " + furExpression + " fur. Moreover, it has 2 wings and can fly.";
+
+        return description;
+    }
+
+    public Bird() {
+        super("blue", 2, false);
+    }
+
 
 }
